8fbe83633cc6fdf57cf6d4a98a49058bdadf5667
syedishmam <syedishmam17@gmail.com>
2020-07-12 13:43:31 -0400

    Bug Fux: Event Target type

--- pokedex/src/components/home/pokemon-search/PokemonSearch.tsx
+++ pokedex/src/components/home/pokemon-search/PokemonSearch.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 
 import SearchBar from './subcomponents/SearchBar';
 import {fetchPokemon} from '../../../api/pokeApiFunctions';
@@ -9,15 +9,16 @@ class PokemonSearch extends React.Component {
         searchQuery: ''
     }
 
-    updateSearchQuery(event: KeyboardEvent) {
-        this.setState({searchQuery: event.target.value})
+    updateSearchQuery = (e: React.FormEvent<HTMLInputElement>): void => {
+        this.setState({searchQuery: e.currentTarget.value});
+        console.log(e.currentTarget.value);
     }
 
     render() {
         return (
             <div>
                 <h3>Search any Pokemon on our database!</h3>
-                <SearchBar fetchPokemon={fetchPokemon}/>
+                <SearchBar fetchPokemon={fetchPokemon} updateSearchQuery={this.updateSearchQuery} searchQuery={this.state.searchQuery}/>
             </div>
         )
     }
--- pokedex/src/components/home/pokemon-search/subcomponents/SearchBar.tsx
+++ pokedex/src/components/home/pokemon-search/subcomponents/SearchBar.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 interface SearchBarProps {
-    fetchPokemon: (searchQuery: string, store: (dataSet: Object[]) => void) => Promise<Object[]>
+    fetchPokemon: (searchQuery: string, store: (dataSet: Object[]) => void) => Promise<Object[]>,
+    updateSearchQuery: (event: any) => void,
+    searchQuery: string
 }
 
 class SearchBar extends React.Component<SearchBarProps> {
     render() {
         return (
             <div>
-                <input type="text"/>
+                <input type="text" onChange={this.props.updateSearchQuery} />
                 <button>Search</button>
             </div>
         )
