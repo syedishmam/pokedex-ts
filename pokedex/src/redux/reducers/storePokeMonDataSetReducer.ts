34be6dbbfd3114f8d4893320dc2a45bb3c3ca16a
syedishmam <syedishmam17@gmail.com>
2020-07-20 16:44:02 -0400

    Add case

--- pokedex/src/redux/reducers/storePokeMonDataSetReducer.ts
+++ pokedex/src/redux/reducers/storePokeMonDataSetReducer.ts
@@ -3,12 +3,16 @@ import { pokemonDataSetAction } from '../actions/actionInterface';
 export default (
   state = {
     pokemonDataSet: '',
+    abilityData: ''
   },
   action: pokemonDataSetAction
 ) => {
   switch (action.type) {
     case 'STORE_POKEMON_DATA_SET':
       return { ...state, pokemonDataSet: action.payload };
+
+    case 'STORE_ABILITY_DATA': 
+      return {...state, abilityData: action.payload}
 
     default:
       return state;
