b4bae2d970f5bf8a730adf735e128b856359824c
syedishmam <syedishmam17@gmail.com>
2020-07-20 16:42:15 -0400

    Refactor, add fetchAbility()

--- pokedex/src/api/pokeApiFunctions.ts
+++ pokedex/src/api/pokeApiFunctions.ts
@@ -1,18 +1,35 @@
 import pokeApi from './pokeApi';
-import { storePokemonDataSet } from '../redux/actions/storePokemonDataSet';
+
+import {abilityCodes} from '../api/abilityCodes';
+import {storePokemonDataSet, storeAbilityData} from '../redux/actions/storePokemonDataSet';
 
 export const fetchPokemon = async (
   searchQuery: string,
-  store: (dataSet: Object[]) => Object
 ): Promise<Object[]> => {
   const pokeData = await pokeApi.get(`pokemon/${searchQuery}`);
   const speciesData = await pokeApi.get(`pokemon-species/${pokeData.data.id}`);
   const dataSet = [pokeData.data, speciesData.data];
-  store(dataSet);
+  storePokemonDataSet(dataSet);
+  console.log(dataSet);
   return dataSet;
 };
 
 export const fetchAbility = async (
   searchQuery: string,
-  store: (data: Object) => Object
-) => {};
+): Promise<Object> => {
+  const abilityData = await pokeApi.get(`type/${searchQuery}`)
+  storeAbilityData(abilityData);
+  console.log(abilityData);
+  return abilityData.data;
+};
+
+export const fetchApiData = async (searchQuery: string) => {
+  let data: Promise<Object>;
+  if(searchQuery in abilityCodes) {
+    data = fetchAbility(searchQuery);
+    return data;
+  } else {
+    data = fetchPokemon(searchQuery);
+    return data;
+  }
+}
